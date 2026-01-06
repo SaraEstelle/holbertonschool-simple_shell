1502e77e375c09efde6f661f37040069e2e90ad2
Ali731-Amb <12252@holbertonstudents.com>
2026-01-06 14:55:11 +0100

    modif_shelle.h

--- shell.h
+++ shell.h
@@ -10,6 +10,11 @@
 
 
 extern char **environ;
+int _strlen(char *s);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strchr(char *s, char c);
 
 
 #endif
