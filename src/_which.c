c9947d5cb5113becfd55850b45eac17631cc38a8
SaraEstelle <12152@holbertonstudents.com>
2026-01-06 16:43:44 +0100

    Adding _which function that search a command in path

---
+++ src/_which.c
@@ -0,0 +1,48 @@
+#include "simple_shell.h"
+#include <sys/stat.h>
+
+/**
+ * _which - Finds full path of a command using PATH
+ * @command: command to find
+ * @path_env: PATH environment variable string
+ * Return: malloc'ed full path, or NULL if not found
+ */
+char *_which(const char *command, const char *path_env)
+{
+	char *path_copy, *dir, *full_path;
+	struct stat st;
+	size_t len;
+
+	if (command == NULL || path_env == NULL)
+		return (NULL);
+
+	path_copy = _strdup((char *)path_env);
+	if (path_copy == NULL)
+		return (NULL);
+
+	dir = strtok(path_copy, ":");
+	while (dir != NULL)
+	{
+		len = _strlen(dir) + 1 + _strlen((char *)command) + 1;
+		full_path = malloc(sizeof(char) * len);
+		if (full_path == NULL)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+
+		snprintf(full_path, len, "%s/%s", dir, command);
+
+		if (stat(full_path, &st) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (NULL);
+}
