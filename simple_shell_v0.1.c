0748c627b39e59ed29b4e874a1c7ddee0876010a
SaraEstelle <12152@holbertonstudents.com>
2026-01-06 17:38:26 +0100

    correcting the header file

--- simple_shell_v0.1.c
+++ simple_shell_v0.1.c
@@ -1,16 +1,16 @@
-#include "shell.h"
+#include "simple_shell.h"
 
 /**
  *
  *
  */
 
-//prototype 
+// prototype
 /* 1. Déclarations (line = NULL, len = 0, argv_command[2]...) */
 
 while (1) /* La boucle infinie */
 {
-char *caractere = NULL;
+	char *caractere = NULL;
 	size_t lenght = 0;
 	ssize_t bytes_read = 0;
 
@@ -26,30 +26,30 @@ char *caractere = NULL;
 	printf("%s", caractere);
 	free(caractere);
 
-    /* 5. Nettoyage : remplacer le '\n' par '\0' à l'index [nread - 1] */
+	/* 5. Nettoyage : remplacer le '\n' par '\0' à l'index [nread - 1] */
 
-	argv[0] = line; 
-	argv[1] = NULL; 
+	argv[0] = line;
+	argv[1] = NULL;
 
-	child_pid = fork();    /* 7. Création du fils avec fork() */
+	child_pid = fork(); /* 7. Création du fils avec fork() */
 	if (child_pid == -1)
 	{
 		perror("Error :");
 		return (1);
 	}
-	if (child_pid == 0)/* 8. DANS LE FILS (if pid == 0) :
-          Appeler execve(argv_command[0], argv_command, environ);
-          SI on arrive ici, c'est que ça a échoué -> perror(av[0]); exit(1);
-    */
+	if (child_pid == 0) /* 8. DANS LE FILS (if pid == 0) :
+		   Appeler execve(argv_command[0], argv_command, environ);
+		   SI on arrive ici, c'est que ça a échoué -> perror(av[0]); exit(1);
+	 */
 
 	{
 		execve(argv_command[0], argv_command, NULL);
 		perror("Execve Failled !!");
 		return (1);
 	}
-	else  /* 9. DANS LE PERE (else) :
-          Appeler wait(&status);
-    */
+	else /* 9. DANS LE PERE (else) :
+		 Appeler wait(&status);
+   */
 	{
 		wait(&status);
 		printf(" Enfant %d a fini son travail\n", i);
