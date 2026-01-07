31a09b4b592b095807ebd518c0d09834c43221b1
Ali731-Amb <12252@holbertonstudents.com>
2026-01-07 19:11:37 +0100

    change_branch_Alison

--- main.c
+++ main.c
@@ -1,23 +1,93 @@
 #include "simple_shell.h"
 
 /**
- * main - entry point of the shell v0.3
- * @argc: argument count (unused)
- * @argv: program name and arguments
- * @envp: environment variables
+ * main - Simple Shell v0.1
+ * @argc: argument count
+ * @argv: argument vector
+ * @envp: environment
  * Return: 0
  */
 int main(int argc, char **argv, char **envp)
 {
-	simple_shell_t shell_state;
+	char *line;
+	char *args[64];
+	char *cmd_path;
+	pid_t pid;
+	int status, arg_count;
+	struct stat st;
 
 	(void)argc;
 
-	/* Détecter si le shell est interactif */
-	shell_state.is_interactive = isatty(STDIN_FILENO);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-	/* Lancer la boucle principale du shell */
-	run_shell(&shell_state, argv, envp);
+		line = read_line();
+		if (!line)
+			break;
 
+		/* Parse la ligne en arguments */
+		arg_count = parse_args(line, args);
+		arg_count++;
+		if (arg_count == 0 || args[0] == NULL ||args[0][0] == '\0' )/*changement dernier condition*/
+		{
+			free(line);
+			continue;
+		}
+
+		/* Si la commande contient '/', c'est un chemin direct */
+		if (_strchr(args[0], '/') != NULL)
+		{
+			if (stat(args[0], &st) != 0)
+			{
+				fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args[0]);
+				free(line);
+	/*rajout */ if(!isatty (STD))
+				continue;
+			}
+			cmd_path = args[0];
+		}
+		else
+		{
+			/* Chercher dans PATH */
+			cmd_path = _which(args[0], my_getenv("PATH", envp));
+			if (cmd_path == NULL || cmd_path[0] == '\0')
+			{
+				fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args[0]);
+				free(line);
+				exit(127);
+			}
+			/*voir pour le chemin path vide*/
+		}
+
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execve(cmd_path, args, envp) == -1)
+			{
+				perror(argv[0]);
+				if (cmd_path != args[0])
+					free(cmd_path);
+				free(line);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if (pid > 0)
+		{
+			wait(&status);
+			if (cmd_path != args[0])
+				free(cmd_path);
+		}
+		else
+		{
+			perror("fork");
+			if (cmd_path != args[0])
+				free(cmd_path);
+		}
+
+		free(line);
+	}
 	return (0);
 }
+/*prbleme de ligne a recouper */
